2cb48d1ca7d183b4608a6db0dd2bbbcedc78a91f
looneym <looneymicheal@gmail.com>
2018-09-29 21:52:00 +0100

    ddd

--- install.sh
+++ install.sh
@@ -10,9 +10,5 @@ for file in $files; do
     ln -sf $dir/$file ~/$file
 done
 
-theme_url=https://raw.githubusercontent.com/wesbos/Cobalt2-iterm/master/cobalt2.zsh-theme
-curl $theme_url >> ~/.oh-my-zsh/themes/cobalt2.zsh-theme
-# pip install --user powerline-status
-# git clone https://github.com/powerline/fonts
-# cd fonts
-# ./install.sh
+ln -sf looneym.zsh-theme ~/.oh-my-zsh/themes/looneym.zsh-theme
+
