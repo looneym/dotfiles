94a90a5d21c3bb0ccd455d3dcdd659301f0324c1
Micheal Looney <micheal.looney@intercom.io>
2019-06-10 14:20:27 +0100

    plug" "

--- install.sh
+++ install.sh
@@ -37,7 +37,7 @@ install_powerline_fonts(){
 symlink_files(){
   touch ~/.transient
   dir=~/dotfiles                    
-  files=".zshrc .vimrc .vim .tmux.conf .gitconfig .gitignore.global .aliases"       
+  files=".zshrc .vimrc .tmux.conf .gitconfig .gitignore.global .aliases"       
   for file in $files; do
       echo "Creating symlink to $file in home directory."
       ln -sf $dir/$file ~/$file
